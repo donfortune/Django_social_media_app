dc7793a11947e2634a811e0cb1d2191fd27376c7
donfortune <donfortunet.df@gmail.com>
2023-07-30 21:39:03 +0100

    adjusted template files

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'home.html')
+    return render(request, 'main.html')
 
 def rooms(request):
     return render(request, 'rooms.html')
